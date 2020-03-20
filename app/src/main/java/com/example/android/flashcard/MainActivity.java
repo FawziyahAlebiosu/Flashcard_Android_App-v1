8527e7ab7ad09bd2211705667dd4168b0e97596d
FawziyahAlebiosu <fawziyah.alebiosu@gmail.com>
2020-03-20 14:38:22 -0400

    completed the required features for lab 3

--- app/src/main/java/com/example/android/flashcard/MainActivity.java
+++ app/src/main/java/com/example/android/flashcard/MainActivity.java
@@ -7,13 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+
+    int currentCardDisplayedIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
 
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            //if our deck of saved cards has at least one card, then call the card
+
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer()); }
         //android studio wil complain if block of code is not inside oncreate method
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,8 +98,57 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //create on click listener for when edit button is clicked, grab the currently displayed question and answer strings,
+        //then pass them to addcard activity to be populated inside of editText
+        findViewById(R.id.editbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //button is clicked, grab the question inside of textView
+                Intent intent = new Intent(MainActivity.this, addCardActivity.class);
+                //create intent to link edit button to addcard activity
+                TextView answer = findViewById(R.id.flashcard_answer);
+                String editAnswer = answer.getText().toString();
 
+                TextView question =  findViewById(R.id.flashcard_question);
+                String editQuestion = question.getText().toString();
+
+                TextView choice = findViewById(R.id.optionOne);
+                String editChoice1 = choice.getText().toString();
+
+                TextView choice2 = findViewById(R.id.optionTwo);
+                String editChoice2 = choice2.getText().toString();
+               intent.putExtra("stringKey1",editAnswer);
+                intent.putExtra("stringKey2", editQuestion);
+                intent.putExtra("stringKey3", editChoice1 );
+                
+                intent.putExtra("stringKey4",editChoice2);
+
+
+
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+        findViewById(R.id.nextbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                ((TextView) findViewById(R.id.answer_side)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                findViewById(R.id.answer_side).setVisibility((View.INVISIBLE));
+                findViewById(R.id.flashcard_question).setVisibility((View.VISIBLE));
+            }
+        });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -93,12 +157,28 @@ public class MainActivity extends AppCompatActivity {
             String string1 = data.getExtras().getString("string1");
             //again it should match the key we defined in the other method
             String string2 = data.getExtras().getString("string2");
+            String string3 = data.getExtras().getString("string3");
+            String string4 = data.getExtras().getString("string4");
             //now send those answers to the text views
             ((TextView)findViewById(R.id.flashcard_question)).setText(string1);
             ((TextView)findViewById(R.id.flashcard_answer)).setText(string2);
+            ((TextView) findViewById(R.id.answer_side)).setText(string2);
+            ((TextView) findViewById(R.id.optionOne)).setText(string3);
+            ((TextView) findViewById(R.id.optionTwo)).setText(string4);
+            //displays card was created successfully
+            Snackbar.make(findViewById(R.id.flashcard_question),
+                    "Card created successfully",
+                    Snackbar.LENGTH_LONG)
+                    .show();
+            findViewById(R.id.editbtn).setVisibility(View.VISIBLE);
+
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+            allFlashcards = flashcardDatabase.getAllCards();
+
+        }
         }
 
     }
-}
+
 
 
--- app/src/main/java/com/example/android/flashcard/addCardActivity.java
+++ app/src/main/java/com/example/android/flashcard/addCardActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class addCardActivity extends AppCompatActivity {
 
@@ -21,6 +23,15 @@ public class addCardActivity extends AppCompatActivity {
                 finish();
             }
         });
+        String editAnswer = getIntent().getStringExtra("stringKey1");
+        String editQuestion = getIntent().getStringExtra("stringKey2");
+        String editChoiceOne = getIntent().getStringExtra("stringKey3");
+        String editChoiceTwo = getIntent().getStringExtra("stringKey4");
+
+        ((EditText)findViewById(R.id.enterQuestion)).setText(editQuestion);
+        ((EditText)findViewById(R.id.enterAnswer)).setText(editAnswer);
+        ((EditText)findViewById(R.id.choiceOne)).setText(editChoiceOne);
+        ((EditText)findViewById(R.id.choiceTwo)).setText(editChoiceTwo);
         //onactivity result method will allow us to get the data we passed into addcard back
 
 
@@ -31,22 +42,34 @@ public class addCardActivity extends AppCompatActivity {
                 String question = ((EditText) findViewById(R.id.enterQuestion)).getText().toString();
                 //and grab input from the answer
                 String answer = ((EditText) findViewById(R.id.enterAnswer)).getText().toString();
-
+                String optionOne = ((EditText) findViewById(R.id.choiceOne)).getText().toString();
+                String optionTwo = ((EditText) findViewById(R.id.choiceTwo)).getText().toString();
                 //create intent to begin passing the data
                 Intent data = new Intent();
                 //now put a string into the data with the key titled string1
-                data.putExtra("string1", question );
+                data.putExtra("string1", question);
                 //do the same again but with key called string 2
                 data.putExtra("string2", answer);
+                data.putExtra("string3", optionOne);
+                data.putExtra("string4", optionTwo);
                 //set the result and bundle to send the data back
-                setResult(RESULT_OK, data);
-                //dismiss this activity, and send data off to mainactivity
+                if (question.matches("") || answer.matches("")) {
+                    //Create toast by initializing
+                    Toast toast = Toast.makeText(getApplicationContext(), "question and answer cannot be blank", Toast.LENGTH_SHORT);
 
-                finish();
+                    //Configure the position of a toast
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else {
+                    //grab input from the answer
+
+                    setResult(RESULT_OK, data);
+                    //dismiss this activity, and send data off to mainactivity
+
+                    finish();
+                }
+
             }
-
-            String answer = getIntent().getStringExtra("string1");
-            String question = getIntent().getStringExtra("string2");
         });
 
     }
